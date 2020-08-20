f16770e61e0d006119d9663ba49eea2b401d2349
senepa <senepa.r@gmail.com>
2020-08-20 12:08:27 -0700

    add giveaway controller

---
+++ gui/app/controllers/giveawaysController.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { indexOf } = require("angular-route");
+
+(function () {
+    angular
+        .module("firebotApp")
+        .controller("giveawaysController", function ($scope, giveawaysService, utilityService) {
+            $scope.giveawaysService = giveawaysService;
+
+            $scope.openEditGiveawaySettingsModal = function (giveaway) {
+                utilityService.showModal({
+                    component: "editGiveawaySettingsModal",
+                    windowClass: "no-padding-modal",
+                    resolveObj: {
+                        giveaway: () => giveaway
+                    },
+                    closeCallback: resp => {
+                        const action = resp.action;
+
+                        if (action === 'save') {
+                            const updatedGiveaway = resp.giveaway;
+                            if (updatedGiveaway == null) return;
+                            giveawaysService.saveGiveaway(updatedGiveaway);
+                        }
+
+                        if (action === 'reset') {
+                            giveawaysService.resetGiveawayToDefault(resp.giveawayId);
+                        }
+                    }
+                });
+            };
+        });
+}());
