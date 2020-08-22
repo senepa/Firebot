36f268dea62a6eeb7520d286b8eeb3970522869c
senepa <senepa.r@gmail.com>
2020-08-21 20:58:25 -0700

    remove raffle commands, migration to gui control

--- backend/giveaways/builtin/raffle/raffle-command.js
+++ backend/giveaways/builtin/raffle/raffle-command.js
@@ -43,120 +43,11 @@ const raffleCommand = {
         id: RAFFLE_COMMAND_ID,
         name: "Raffle",
         active: true,
-        trigger: "!raffle",
+        trigger: "!enter",
         description: "Allows viewers to participate in a raffle.",
         autoDeleteTrigger: false,
         scanWholeMessage: false,
-        hideCooldowns: true,
-        subCommands: [
-            {
-                id: "raffleStart",
-                arg: "start",
-                usage: "start [manual | currency]",
-                description: "Starts a raffle.",
-                hideCooldowns: true,
-                restrictionData: {
-                    restrictions: [
-                        {
-                            id: "sys-cmd-mods-only-perms",
-                            type: "firebot:permissions",
-                            mode: "roles",
-                            roleIds: [
-                                "Mod",
-                                "ChannelEditor",
-                                "Owner"
-                            ]
-                        }
-                    ]
-                },
-                subCommands: [
-                    {
-                        id: "raffleManual",
-                        arg: "manual",
-                        usage: "manual",
-                        description: "Starts a raffle for users in chat using manual entry.",
-                        hideCooldowns: true,
-                        restrictionData: {
-                            restrictions: [
-                                {
-                                    id: "sys-cmd-mods-only-perms",
-                                    type: "firebot:permissions",
-                                    mode: "roles",
-                                    roleIds: [
-                                        "Mod",
-                                        "ChannelEditor",
-                                        "Owner"
-                                    ]
-                                }
-                            ]
-                        }
-                    },
-                    {
-                        id: "raffleCurrency",
-                        arg: "currency",
-                        usage: "currency",
-                        description: "Starts a raffle for users in chat using a currency system.",
-                        hideCooldowns: true,
-                        restrictionData: {
-                            restrictions: [
-                                {
-                                    id: "sys-cmd-mods-only-perms",
-                                    type: "firebot:permissions",
-                                    mode: "roles",
-                                    roleIds: [
-                                        "Mod",
-                                        "ChannelEditor",
-                                        "Owner"
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                id: "raffleStop",
-                arg: "stop",
-                usage: "stop",
-                description: "Manually stops the raffle. Selects a winner.",
-                hideCooldowns: true,
-                restrictionData: {
-                    restrictions: [
-                        {
-                            id: "sys-cmd-mods-only-perms",
-                            type: "firebot:permissions",
-                            mode: "roles",
-                            roleIds: [
-                                "Mod",
-                                "ChannelEditor",
-                                "Owner"
-                            ]
-                        }
-                    ]
-                }
-            },
-            {
-                id: "raffleClear",
-                arg: "clear",
-                usage: "clear",
-                description: "Clears the raffle currency for all users.",
-                hideCooldowns: true,
-                restrictionData: {
-                    restrictions: [
-                        {
-                            id: "sys-cmd-mods-only-perms",
-                            type: "firebot:permissions",
-                            mode: "roles",
-                            roleIds: [
-                                "Mod",
-                                "ChannelEditor",
-                                "Owner"
-                            ]
-                        }
-                    ]
-                }
-            }
-        ]
+        hideCooldowns: true
     },
     onTriggerEvent: async event => {
         const { chatEvent, userCommand } = event;
@@ -173,95 +64,6 @@ const raffleCommand = {
             twitchChat.sendChatMessage("Unable to start a raffle as the selected currency appears to not exist anymore.", null, chatter);
             twitchChat.deleteMessage(chatEvent.id);
         }
-
-        if (event.userCommand.subcommandId === "raffleStart") {
-            const username = userCommand.commandSender;
-
-            if (activeBiddingInfo.active !== false) {
-                twitchChat.sendChatMessage(`There is already a raffle running. Use !raffle stop to stop it.`, username, chatter);
-                twitchChat.deleteMessage(chatEvent.id);
-                return;
-            }
-
-            // When "!raffle start manual" is called, chat users use "!enter" to enter the raffle
-            // After timeLimit is reached, a winner is randomly selected from the list of entered users.
-            if (event.userCommand.subcommandId === "raffleManual") {
-
-                let timeLimit = raffleSettings.settings.manualSettings.startDelay * 60000;
-
-                activeRaffleInfo = {
-                    "active": true
-                };
-
-                bidTimer = setTimeout(function () {
-                    stopRaffle(chatter);
-                }, timeLimit);
-
-                raffleRunner.triggerLobbyStart(startDelay);
-
-                // When "!raffle start currency" is called, the currency of all users is totalled.
-                // A random number is picked between 0 and the currency total, called the winning number.
-                // A loop iterates through all currency holders, subtracting their currency amounts from the winning number.
-                // When the winning number hits 0, that user is selected as the winner and announced to chat
-            } else if (event.userCommand.subcommandId === "raffleCurrency") {
-
-                let timeLimit = raffleSettings.settings.currencySettings.startDelay * 60000;
-
-                activeRaffleInfo = {
-                    "active": true
-                };
-
-                bidTimer = setTimeout(function () {
-                    stopRaffle(chatter);
-                }, timeLimit);
-
-                raffleRunner.triggerLobbyStart(startDelay);
-
-            }
-
-
-            // raffleStop stops a manual raffle early
-        } else if (event.userCommand.subcommandId === "raffleStop") {
-
-            stopRaffle(chatter);
-
-            // raffleClear removes all of the currency from all holders
-        } else if (event.userCommand.subcommandId === "raffleClear") {
-
-            await currencyDatabase.purgeCurrencyById(currencyId);
-
-        } else {
-            twitchChat.sendChatMessage(`Incorrect raffle usage: ${userCommand.trigger}`, userCommand.commandSender, chatter);
-            twitchChat.deleteMessage(chatEvent.id);
-        }
-    }
-};
-
-const raffleEnterCommand = {
-    definition: {
-        id: ENTER_COMMAND_ID,
-        name: "Enter",
-        active: true,
-        trigger: "!enter",
-        description: "Allows a user to enter a raffle.",
-        autoDeleteTrigger: false,
-        scanWholeMessage: false,
-        hideCooldowns: true,
-    },
-    onTriggerEvent: async event => {
-
-        const { chatEvent, userCommand } = event;
-
-        const username = userCommand.commandSender;
-
-        const raffleSettings = giveawayManager.getGiveawaySettings("firebot-raffle");
-
-        const chatter = bidSettings.settings.chatSettings.chatter;
-
-        const currencyId = bidSettings.settings.currencySettings.currencyId;
-        const currency = currencyDatabase.getCurrencyById(currencyId);
-        const currencyName = currency.name;
-
         // Ensure the raffle has been started and the lobby ready
         if (raffleRunner.lobbyOpen) {
 
@@ -273,7 +75,6 @@ const raffleEnterCommand = {
             });
 
         }
-
     }
 };
 
@@ -307,9 +108,6 @@ function registerRaffleCommand() {
     if (!commandManager.hasSystemCommand(RAFFLE_COMMAND_ID)) {
         commandManager.registerSystemCommand(raffleCommand);
     }
-    if (!commandManager.hasSystemCommand(ENTER_COMMAND_ID)) {
-        commandManager.registerSystemCommand(raffleEnterCommand);
-    }
     if (!commandManager.hasSystemCommand(CLAIM_COMMAND_ID)) {
         commandManager.registerSystemCommand(raffleClaimCommand);
     }
@@ -317,7 +115,6 @@ function registerRaffleCommand() {
 
 function unregisterRaffleCommand() {
     commandManager.unregisterSystemCommand(RAFFLE_COMMAND_ID);
-    commandManager.unregisterSystemCommand(ENTER_COMMAND_ID);
     commandManager.unregisterSystemCommand(CLAIM_COMMAND_ID);
 }
 
