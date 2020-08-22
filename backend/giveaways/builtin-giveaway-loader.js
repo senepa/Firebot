7de41706adf8b33444647f6270e033c482f6eb4c
senepa <senepa.r@gmail.com>
2020-08-21 20:30:36 -0700

    remove lottery giveaway

--- backend/giveaways/builtin-giveaway-loader.js
+++ backend/giveaways/builtin-giveaway-loader.js
@@ -5,5 +5,4 @@ const giveawayManager = require("./giveaway-manager");
 exports.loadGiveaways = () => {
     giveawayManager.registerGiveaway(require("./builtin/bid/bid"));
     giveawayManager.registerGiveaway(require("./builtin/raffle/raffle"));
-    giveawayManager.registerGiveaway(require("./builtin/lottery/lottery"));
 };
